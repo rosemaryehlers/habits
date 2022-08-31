f4f205e0e1fe80c537cd388ede1d09e367982347
unknown <rosemary.ehlers@gmail.com>
2022-08-31 15:46:18 -0600

    convert

--- frontend/src/History.tsx
+++ frontend/src/History.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import Alert from 'react-bootstrap/Alert';
-import Button from 'react-bootstrap/Button';
+import React, { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
@@ -28,97 +26,53 @@ interface Item {
     status: ItemStatus;
     threshold?: ItemThreshold;
 }
-interface HistoryState {
-    errorMsg?: string;
-    items: Array<Item>;
-    loadedItemHistories: Array<number>;
-}
 
+function History(props: HistoryProps){
+    const [items, setItems] = useState<Array<Item>>([]);
+    const [loadedHistories, setLoadedHistories] = useState<Array<number>>([]);
+    const historyPath = "/history";
 
-interface ItemHistoryEntry {
-    dueDate: string;
-    count: number;
-}
-interface ItemHistoryMetadata {
-    name: string;
-    type: string;
-    goal?: number;
-}
-
-const weeks = 6;
-const historyPath = "/history";
-
-class History extends React.Component<HistoryProps, HistoryState> {
-    constructor(props: HistoryProps){
-        super(props);
-
-        this.state = {
-            items: [],
-            loadedItemHistories: []
-        };
-    }
-
-    onItemHistoryClick(e: any, itemId: number) {
-        if(this.state.loadedItemHistories.includes(itemId)){
-            e.preventDefault(); // history already loaded, do nothing
+    // fetch view history
+    useEffect(() => {
+        console.log("History useEffect, " + props.selectedView);
+        if(props.selectedView === undefined){
             return;
         }
 
-        let newHistories = Object.assign([], this.state.loadedItemHistories);
-        newHistories.push(itemId);
-        this.setState({
-            loadedItemHistories: newHistories
-        });
-    }
-
-    fetchItems(){
-        if(this.props.selectedView === undefined){
-            console.log("No view to fetch items for.");
-            return;
-        }
-
-        var url = this.props.global.baseUrl + 
-            ":" + this.props.global.port + 
-            historyPath + "?view=" + this.props.selectedView;
+        var url = props.global.baseUrl + 
+            ":" + props.global.port + 
+            historyPath + "?view=" + props.selectedView;
 
         fetch(url, { method: "GET" }).then(resp => {
             if(!resp.ok){
-                console.log(`Error ${resp.status} fetching history for view ${this.props.selectedView}: ${resp.statusText}`);
-                this.setState({
-                    errorMsg: "Error fetching history.",
-                });
+                console.log(`Error ${resp.status} fetching history for view ${props.selectedView}: ${resp.statusText}`);
+                props.global.showErrorAlert("Error fetching history");
                 return undefined;
             } else {
                 return resp.json();
             }
         }).then(data => {
             if(data !== undefined){
-                this.setState({
-                    items: data.items,
-                    errorMsg: undefined
-                });
+                setItems(data.items);
             }
         }).catch(err => {
-            console.log(`Error fetching history for view ${this.props.selectedView}: ${err}`);
-            this.setState({
-                errorMsg: "Error fetching history."
-            });
+            console.log(`Error fetching history for view ${props.selectedView}: ${err}`);
+            props.global.showErrorAlert("Error fetching history");
         });
-    }
+    }, [props.selectedView]);
 
-    componentDidMount() {
-        // default is to the all-items view
-        this.fetchItems();
-    }
-    componentDidUpdate(prevProps: HistoryProps, prevState: HistoryState) {
-        if (this.props.selectedView !== prevProps.selectedView){
-            // if the view has changed, the default is to show the all-items for a given view
-            // so no need to handle single-item history view, we are moving away from it
-            this.fetchItems();
+    function onItemHistoryClick(e: any, itemId: number) {
+        if(loadedHistories.includes(itemId)){
+            e.preventDefault(); // history already loaded, do nothing
+            return;
         }
+
+        let newHistories = Object.assign([], loadedHistories);
+        newHistories.push(itemId);
+        setLoadedHistories(newHistories);
     }
 
-    renderItemSuccess(item: Item){
+    function renderItemSuccess(item: Item){
         if(item.type === "finite" && item.status.goal !== undefined){
             let percentage = Math.floor( (item.status.count / item.status.goal) * 100 );
             let color = "";
@@ -143,34 +97,34 @@ class History extends React.Component<HistoryProps, HistoryState> {
         return ("Unknown item type.");
     }
 
-    renderItemHistory(item: Item){
-        if(this.state.loadedItemHistories.includes(item.id)){
-            return <ItemHistory itemId={item.id} global={this.props.global} />;
+    function renderItemHistory(item: Item){
+        if(loadedHistories.includes(item.id)){
+            return <ItemHistory itemId={item.id} global={props.global} />;
         }
     }
 
-    render(){
-        return (
-            <Container fluid className="history page-content">
-                <Row hidden={this.state.items !== undefined && this.state.items.length > 0}>
-                    No items found!
-                </Row>
-                <Accordion className="left row" flush>
-                { this.state.items.map(item => (
-                    <Accordion.Item eventKey={item.id + ""} key={item.id}>
-                        <Accordion.Header onClick={ (e) => { this.onItemHistoryClick(e, item.id); } }>
-                            <Col className="left">{item.name}</Col>
-                            <Col className="right">{this.renderItemSuccess(item)}</Col>
-                        </Accordion.Header>
-                        <Accordion.Body id={item.id + ""}>
-                            { this.renderItemHistory(item) }
-                        </Accordion.Body>
-                    </Accordion.Item>
-                )) }
-                </Accordion>
-            </Container>
-        );
+    if(items.length === 0){
+        return <Row>No items found!</Row>
     }
+
+    return (
+        <Container fluid className="history page-content">
+            <Accordion className="left row" flush>
+            { items.map(item => (
+                <Accordion.Item eventKey={item.id + ""} key={item.id}>
+                    <Accordion.Header onClick={ (e) => { onItemHistoryClick(e, item.id); } }>
+                        <Col className="left">{item.name}</Col>
+                        <Col className="right">{ renderItemSuccess(item) }</Col>
+                    </Accordion.Header>
+                    <Accordion.Body id={item.id + ""}>
+                        { renderItemHistory(item) }
+                    </Accordion.Body>
+                </Accordion.Item>
+            )) }
+            </Accordion>
+        </Container>
+    );
+
 }
 
 export default History;
