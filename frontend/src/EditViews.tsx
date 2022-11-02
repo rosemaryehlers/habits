fc860241eb1585fd29f24cda04102f24d760baff
rosemaryehlers <rosemary.ehlers@gmail.com>
2022-11-01 18:52:26 -0600

    werk

--- frontend/src/EditViews.tsx
+++ frontend/src/EditViews.tsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
-import { Button, Col, Container, Dropdown, Form, Modal, Row, Stack } from 'react-bootstrap';
+import { Button, Col, Container, Dropdown, Form, InputGroup, Modal, Row, Stack } from 'react-bootstrap';
 import './Navigation.css';
+import './Edit.css';
 import iconback from 'bootstrap-icons/icons/arrow-left.svg';
 import { GlobalProps } from './GlobalProps';
+
+interface ViewData {
+    name: string;
+    tasks: Array<string>;
+}
 
 function EditViews(props: GlobalProps) {
     const temp = [...props.global.views];
@@ -25,7 +31,10 @@ function EditViews(props: GlobalProps) {
                                 {
                                     viewList.filter( v => v !== selectedView ).map(view => {
                                         return (
-                                            <Dropdown.Item value={view} key={view} onClick={onViewChange} >{view}</Dropdown.Item>
+                                            <>
+                                                <Dropdown.Item value={view} key={view} onClick={onViewChange} >{view}</Dropdown.Item>
+                                                { view === "Create New" && <Dropdown.Divider></Dropdown.Divider> }
+                                            </>
                                         );
                                     })
                                 }
@@ -41,10 +50,24 @@ function EditViews(props: GlobalProps) {
             </Container>
             <Container fluid className="edit">
                 { selectedView === "Create New" &&
-                    <div>Create new form</div>
+                    <div className="edit-form-container">
+                        <div className="edit-form">
+                            <InputGroup className="name">
+                                <InputGroup.Text id="name">Name</InputGroup.Text>
+                                <Form.Control aria-label="Name" aria-describedby="name"></Form.Control>
+                            </InputGroup>
+                        </div>
+                        <div className="submit-container">
+                            <Button variant="success">Create</Button>{' '}
+                        </div>
+                    </div>
                 }
                 { selectedView !== "Create New" &&
-                    <div>Edit {selectedView}</div>
+                    <div className="edit-form-container">
+                        <div className="edit-form">
+                            <div>Editing {selectedView}</div>
+                        </div>
+                    </div>
                 }
             </Container>
         </div>
